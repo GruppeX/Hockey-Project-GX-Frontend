08827b212c1bcef2b2a7a4c1e1068f73d7d5dc16
vitaliy-serdyukov <82437169+vitaliy-serdyukov@users.noreply.github.com>
2022-05-10 13:14:02 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,6 +22,3 @@ const SweeperColor = "#2A9D8F";
 const MidfieldersColor = "#0077B6";
 const AttackerColor = "#E76F51";
 const DefenderColor = "#264653";
-
-const body = document.getElementById("body");
-body.style.backgroundColor = PrimaryDarkBlue;
