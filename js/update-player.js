8772daa231390dedabeaff197cfbec028d007cd0
Jens-k-m-m <Jens-k-m-m@users.noreply.github.com>
2022-05-10 10:48:09 +0000

    Prettified Code!

--- js/update-player.js
+++ js/update-player.js
@@ -1,7 +1,7 @@
 // Update player
 out("Update player");
 
-let playerUrl = baseUrl + updateUrl + 'player/';
+let playerUrl = baseUrl + updateUrl + "player/";
 
 /**
  * get all players from DB
@@ -11,36 +11,35 @@ let playerUrl = baseUrl + updateUrl + 'player/';
  * @ atu
  */
 async function updatePlayer(player) {
-    out('player - ' + player);
-    playerUrl = playerUrl + player.playerId;
-    out(playerUrl);
+  out("player - " + player);
+  playerUrl = playerUrl + player.playerId;
+  out(playerUrl);
 
-    const jsonString = JSON.stringify(player);
+  const jsonString = JSON.stringify(player);
 
-    const fetchOptions = {
-        method: "PUT",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: jsonString
-    }
-    //calls API (Backend) and wait for return
-    const response = await fetch(playerUrl, fetchOptions);
+  const fetchOptions = {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: jsonString,
+  };
+  //calls API (Backend) and wait for return
+  const response = await fetch(playerUrl, fetchOptions);
 
-    out(response)
-    if (!response) {
-        alert('Something went wrong')
-        out("Det gik ikke godt med update");
+  out(response);
+  if (!response) {
+    alert("Something went wrong");
+    out("Det gik ikke godt med update");
+  } else {
+    if (response.ok) {
+      alert(player.firstName + " is updated");
     } else {
-        if (response.ok) {
-            alert(player.firstName + ' is updated');
-        } else {
-            alert('Something went wrong\nERROR status: ' + response.status);
-        }
-
+      alert("Something went wrong\nERROR status: " + response.status);
     }
-    return response;
-}// Update player end
+  }
+  return response;
+} // Update player end
 
 /**
  * Test in console testUpdate(3);
@@ -48,12 +47,12 @@ async function updatePlayer(player) {
  * @returns {Promise<void>}
  */
 async function testUpdate(index) {
-    let playerlist = [];
-    await getAllPlayers();
-    //players[index].firstName = 'Jens';
-    out('players - ' + players[index].isSelected);
-    players[index].isSelected = 1;
-    out('players - ' + players[index].isSelected);
+  let playerlist = [];
+  await getAllPlayers();
+  //players[index].firstName = 'Jens';
+  out("players - " + players[index].isSelected);
+  players[index].isSelected = 1;
+  out("players - " + players[index].isSelected);
 
-    await updatePlayer(players[index]);
+  await updatePlayer(players[index]);
 }
