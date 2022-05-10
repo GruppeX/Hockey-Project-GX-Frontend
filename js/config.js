2daeec22a308a1c8450bf7ce937cf062787549c3
vital <vitaliy_serdyukov@hotmail.com>
2022-05-10 09:11:30 +0200

    GXH-8_create_player_added

--- js/config.js
+++ js/config.js
@@ -1,3 +1,8 @@
+// Print console.log
+const out = function (str) {
+    console.log(str);
+}
+
 const baseUrl = "http://localhost:8080/";
 const createUrl = "create/";
 const updateUrl = "update/";
@@ -17,3 +22,6 @@ const SweeperColor = "#2A9D8F";
 const MidfieldersColor = "#0077B6";
 const AttackerColor = "#E76F51";
 const DefenderColor = "#264653";
+
+const body = document.getElementById('body');
+body.style.backgroundColor = PrimaryDarkBlue;
---
+++ js/create-player.js
@@ -0,0 +1,55 @@
+out('er i create player');
+
+//generate api from config file
+const url = baseUrl + createUrl + 'player';
+
+//execute function createFormEventListener when html is loaded
+document.addEventListener('DOMContentLoaded', createFormEventListener);
+
+let playerForm;
+
+//add eventlistener to html form
+function createFormEventListener() {
+    playerForm = document.getElementById('newPlayerForm');
+    playerForm.addEventListener('submit', handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+    //preventDefault forhindrer form i at udføre default submit. altås sende sig selv til backend.
+    event.preventDefault();
+    const form = event.currentTarget;
+    out(url);
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson(url, formData);
+        out('Formdata' + formData)
+        out(responseData);
+        alert(formData.get('playerTitle') + ' er oprettet');
+        playerForm.reset();
+
+    } catch (err) {
+        alert(err.message);
+        out(err);
+    }
+}
+
+async function postFormDataAsJson(url, formData) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    out('Her er vi nu' + plainFormData);
+    const formDataJsonString = JSON.stringify(formData);
+    out("Print2 her" + formDataJsonString);
+
+    const fetchOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: formDataJsonString
+    };
+
+    const response = await fetch(url, fetchOptions);
+    if (!response) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
+}
