4945c1f6d3deb8ade5a173bb1ca72efdc7fc6cd9
jakobmajiddd <jakobmajiddd@users.noreply.github.com>
2022-05-10 11:35:02 +0000

    Prettified Code!

--- js/delete-player.js
+++ js/delete-player.js
@@ -1,4 +1,4 @@
-let deletePlayerUrl = baseUrl + deleteUrl + 'player/';
+let deletePlayerUrl = baseUrl + deleteUrl + "player/";
 
 /**
  * fetch starts a request and returns a promise
@@ -8,23 +8,21 @@ let deletePlayerUrl = baseUrl + deleteUrl + 'player/';
  */
 
 async function deletePlayer(id) {
-    deletePlayerUrl = deletePlayerUrl + id;
-    out(deletePlayerUrl);
+  deletePlayerUrl = deletePlayerUrl + id;
+  out(deletePlayerUrl);
 
-    const fetchOptions = {
-        method: "DELETE",
-        headers: {
-            "Content-type": "application/json"
-        }
-    }
-    //calls API (Backend) and wait for return
-    const response = await fetch(deletePlayerUrl, fetchOptions);
-    if (!response) {
-        alert('Something went wrong with delete json');
-    } else {
-        alert('Player is deleted');
-    }
-    return response;
+  const fetchOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+  //calls API (Backend) and wait for return
+  const response = await fetch(deletePlayerUrl, fetchOptions);
+  if (!response) {
+    alert("Something went wrong with delete json");
+  } else {
+    alert("Player is deleted");
+  }
+  return response;
 }
-
-
