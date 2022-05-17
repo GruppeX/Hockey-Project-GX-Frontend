52b47d1313c1c04c41c7c9f2cb3db7c17fcc6909
Dubz97 <Dubz97@users.noreply.github.com>
2022-05-17 10:30:02 +0000

    Prettified Code!

--- js/create-player.js
+++ js/create-player.js
@@ -19,22 +19,19 @@ async function handleFormSubmit(event) {
   //prevent Default prevents from performing default form submit (send itself to the backend)
   event.preventDefault();
   const form = event.currentTarget;
-    out(url);
-    try {
-      const formData = new FormData(form);
-        const responseData = await postFormDataAsJson(url, formData);
-        out(responseData);
-        alert(
-            formData.get("firstName") +
-            " " +
-            formData.get("lastName") +
-            " is created"
-        );
-        playerForm.reset();
-    } catch (err) {
-      alert("Something went wrong here " + err.message);
-      out(err);
-    }
+  out(url);
+  try {
+    const formData = new FormData(form);
+    const responseData = await postFormDataAsJson(url, formData);
+    out(responseData);
+    alert(
+      formData.get("firstName") + " " + formData.get("lastName") + " is created"
+    );
+    playerForm.reset();
+  } catch (err) {
+    alert("Something went wrong here " + err.message);
+    out(err);
+  }
 }
 
 //post formData as JSON
