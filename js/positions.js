cbdf205379e98313b34359728baf44bda130663d
Christoffer <christofferp97@hotmail.com>
2022-05-17 14:14:09 +0200

    GXH-15 created roles on playercards on position change

--- js/positions.js
+++ js/positions.js
@@ -1,6 +1,39 @@
+const rolePos1 = ['Goalkeeper', 'Sweeper', 'Defender', 'Defender', 'Defender', 'Midfielder', 'Midfielder', 'Attacker',
+    'Attacker', 'Attacker', 'Attacker'];
+
+const rolePos2 = ['Goalkeeper', 'Sweeper', 'Defender', 'Defender', 'Defender', 'Defender', 'Midfielder', 'Midfielder',
+    'Attacker', 'Attacker', 'Attacker'];
+
+const rolePos3 = ['Goalkeeper', 'Sweeper', 'Defender', 'Defender', 'Defender', 'Midfielder', 'Midfielder', 'Midfielder',
+    'Midfielder', 'Attacker', 'Attacker'];
+
+
 const ddPositions = document.getElementById("selectedPosition");
+document.addEventListener('DOMContentLoaded', changeRoleOnCards);
+ddPositions.addEventListener('change', changeRoleOnCards);
 
 //Print position
 ddPositions.addEventListener("change", function () {
-  out(ddPositions.value);
+    out(ddPositions.value);
 });
+
+function changeRoleOnCards() {
+    if (ddPositions.value === '1-3-2-4') {
+        for (let i = 0; i < rolePos1.length; i++) {
+            let playerCard = document.getElementById("playerFieldCard" + (i + 1));
+            playerCard.innerText = rolePos1[i];
+        }
+    }
+  if (ddPositions.value === '1-4-2-3') {
+    for (let i = 0; i < rolePos2.length; i++) {
+      let playerCard = document.getElementById("playerFieldCard" + (i + 1));
+      playerCard.innerText = rolePos2[i];
+    }
+  }
+  if (ddPositions.value === '1-3-4-2') {
+    for (let i = 0; i < rolePos3.length; i++) {
+      let playerCard = document.getElementById("playerFieldCard" + (i + 1));
+      playerCard.innerText = rolePos3[i];
+    }
+  }
+}
