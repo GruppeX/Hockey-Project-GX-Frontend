6e370f4d00e578366dcdaf5f1b98e7aa92ef12bf
vitaliy-serdyukov <vitaliy-serdyukov@users.noreply.github.com>
2022-05-24 22:05:47 +0000

    Prettified Code!

--- js/create-player.js
+++ js/create-player.js
@@ -12,8 +12,8 @@ let playerForm;
  */
 
 function createFormEventListener() {
-    playerForm = document.getElementById("createPlayerForm");
-    playerForm.addEventListener("submit", handleFormSubmit);
+  playerForm = document.getElementById("createPlayerForm");
+  playerForm.addEventListener("submit", handleFormSubmit);
 }
 
 /**
@@ -23,21 +23,21 @@ function createFormEventListener() {
  */
 
 async function handleFormSubmit(event) {
-    //prevent Default prevents from performing default form submit (send itself to the backend)
-    event.preventDefault();
-    const form = event.currentTarget;
-    try {
-        const formData = new FormData(form);
-        const responseData = await postFormDataAsJson(url, formData);
-        out(responseData);
-        alert(
-            formData.get("firstName") + " " + formData.get("lastName") + " is created"
-        );
-        playerForm.reset();
-    } catch (err) {
-        alert("Something went wrong here " + err.message);
-        out(err);
-    }
+  //prevent Default prevents from performing default form submit (send itself to the backend)
+  event.preventDefault();
+  const form = event.currentTarget;
+  try {
+    const formData = new FormData(form);
+    const responseData = await postFormDataAsJson(url, formData);
+    out(responseData);
+    alert(
+      formData.get("firstName") + " " + formData.get("lastName") + " is created"
+    );
+    playerForm.reset();
+  } catch (err) {
+    alert("Something went wrong here " + err.message);
+    out(err);
+  }
 }
 
 /**
@@ -49,22 +49,22 @@ async function handleFormSubmit(event) {
  */
 
 async function postFormDataAsJson(url, formData) {
-    const plainFormData = Object.fromEntries(formData.entries());
-    out(plainFormData);
-    const formDataJsonString = JSON.stringify(plainFormData);
-    out(formDataJsonString);
+  const plainFormData = Object.fromEntries(formData.entries());
+  out(plainFormData);
+  const formDataJsonString = JSON.stringify(plainFormData);
+  out(formDataJsonString);
 
-    const fetchOptions = {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: formDataJsonString,
-    };
+  const fetchOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: formDataJsonString,
+  };
 
-    const response = await fetch(url, fetchOptions);
-    if (!response) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
+  const response = await fetch(url, fetchOptions);
+  if (!response) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
 
-    return response;
+  return response;
 }
