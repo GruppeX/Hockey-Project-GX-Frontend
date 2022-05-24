d7a35694f32be82f170012aaafe96d999c45139d
jakobmajiddd <jakobmajid00@gmail.com>
2022-05-24 10:06:23 +0200

    GXH-20 innertext on edit

--- js/update-player.js
+++ js/update-player.js
@@ -1,6 +1,3 @@
-// Update player
-out("Update player");
-
 let playerUrl = baseUrl + updateUrl + "player/";
 
 /**
@@ -8,68 +5,58 @@ let playerUrl = baseUrl + updateUrl + "player/";
  * filter to one player by id, make to a JSON
  * @param player
  * @returns {Promise<Response>} update one player
- * @ atu
+ * @author Jens, Jackie & Jakob
  */
 
 async function updatePlayer(player) {
-  out("player - " + player);
-  playerUrl = playerUrl + player.playerId;
-  out(playerUrl);
+    out("player - " + player);
+    playerUrl = playerUrl + player.playerId;
+    out(playerUrl);
 
-  const jsonString = JSON.stringify(player);
+    const jsonString = JSON.stringify(player);
 
-  const fetchOptions = {
-    method: "PUT",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: jsonString,
-  };
-  //calls API (Backend) and wait for return
-  const response = await fetch(playerUrl, fetchOptions);
+    const fetchOptions = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: jsonString,
+    };
+    //calls API (Backend) and wait for return
+    const response = await fetch(playerUrl, fetchOptions);
 
-  out(response);
-  if (!response) {
-    alert("Something went wrong");
-    out("Det gik ikke godt med update");
-  } else {
-    if (response.ok) {
-      alert(player.firstName + " is updated");
+    out(response);
+    if (!response) {
+        alert("Something went wrong");
+        out("Det gik ikke godt med update");
     } else {
-      alert("Something went wrong\nERROR status: " + response.status);
+        if (response.ok) {
+            alert(player.firstName + " is updated");
+        } else {
+            alert("Something went wrong\nERROR status: " + response.status);
+        }
     }
-  }
-  return response;
+    return response;
 } // Update player end
 
-/**
- * Test in console testUpdate(3);
- * @param index
- * @returns {Promise<void>}
- */
-async function testUpdate(index) {
-  let playerlist = [];
-  await getAllPlayers();
-  //players[index].firstName = 'Jens';
-  out("players - " + players[index].isSelected);
-  players[index].isSelected = 1;
-  out("players - " + players[index].isSelected);
 
-  await updatePlayer(players[index]);
-}
-async function selectId(value){
-out(value+ "value");
-  let player = await getPlayerById(parseInt(value));
-  out(player);
-  let firstNameEdit = document.getElementById("firstNameEdit");
-  firstNameEdit.value = player.firstName;
+async function selectedPlayerById(value) {
+    out(value + "value");
+    let player = await getPlayerById(parseInt(value));
+    out(player);
+    let firstNameEdit = document.getElementById("firstNameEdit");
+    let lastNameEdit = document.getElementById("lastNameEdit");
+    let roleEdit = document.getElementById("roleEdit");
+    firstNameEdit.value = player.firstName;
+    lastNameEdit.value = player.lastName;
+    roleEdit.value = player.role;
 
 }
-function updateButton(){
-  let form = document.getElementById("editPlayerForm");
-  let fromdata = JSON.stringify(form);
-  out(fromdata);
 
+function updateButton() {
+    let form = document.getElementById("editPlayerForm");
+    let fromdata = JSON.stringify(form);
+    out(fromdata);
 
 
 }
