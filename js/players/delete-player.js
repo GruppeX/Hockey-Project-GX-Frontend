439f6532474593923045cbeee352b41d8744eaec
Christoffer <christofferp97@hotmail.com>
2022-05-24 11:06:36 +0200

    GXH-22 needs to be able to delete selected player from list

--- js/players/delete-player.js
+++ js/players/delete-player.js
@@ -1,15 +1,25 @@
-const playerMap1 = new Map();
+const playerMapDelete = new Map();
 const findPlayerNavDelete = document.getElementById("deletePlayerNav");
 
-findPlayerNavDelete.addEventListener("click", createSearchList);
+findPlayerNavDelete.addEventListener("click", createSearchListDelete);
 
+async function createPlayerMapDelete() {
+    const playerList = await getAllPlayers();
+    out(playerList)
+    // sorting our array with players by firstname
+    playerList.sort((a, b) => a.firstName.localeCompare(b.firstName));
 
+    playerList.forEach((player) => {
+        playerMapDelete.set(player.playerId, player);
+    });
+}
 
 
 async function createSearchListDelete() {
-    await createPlayerMap1();
-    let playerList = document.getElementById("selectPlayer");
-    playerMap1.forEach((player) => {
+    await createPlayerMapDelete();
+    out(playerMapDelete + " TJEK HER")
+    let playerList = document.getElementById("deleteSelectPlayer");
+    playerMapDelete.forEach((player) => {
         let li = document.createElement("li");
         li.classList.add("player-list-tile");
         let button = document.createElement("button");
